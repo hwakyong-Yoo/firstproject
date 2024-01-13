606c572fe4f877caa4aa61eb52ab0d356a424e01
hwakyong YOO <hwakyongyoo@gmail.com>
2024-01-13 19:04:13 +0900

    Initial commit

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+//URL 연결 요청(@GetMapping())과 동시에 자동으로 임포트
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //컨트롤러 선언
+public class FirstController {
+
+    @GetMapping("/hi") //URL 요청 접수
+    public String niceToMeetYou(Model model) { //model 객체 받아 오기
+        model.addAttribute("username", "홍팍");
+        return "greetings"; // greetings.mustache 파일 반환
+    }
+}
