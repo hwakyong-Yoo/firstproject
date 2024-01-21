d261a95e6c45dee0026159751e0ecbe9da97adb7
hwakyong YOO <hwakyongyoo@gmail.com>
2024-01-21 18:25:32 +0900

    20240121 롬북으로 코드 간단화

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,7 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 @Entity
 public class Article {
     @Id
@@ -15,18 +19,5 @@ public class Article {
     @Column
     private String Content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        Content = content;
-    }
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", Content='" + Content + '\'' +
-                '}';
-    }
 }
